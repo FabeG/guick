3c18e8e821954739c4f51e5c603ce7cde162079e
GUY Fabrice <fabrice.guy@rte-france.com>
2025-12-21 22:56:18 +0100

    rtd: don't use wx

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,11 @@ release = '0.5'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
 ]
+
+# This prevents Sphinx from actually importing wx
+autodoc_mock_imports = ["wx"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
