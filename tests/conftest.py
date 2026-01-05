88a382ef0ab7478d17e62e7a90a06cb6d68f5f62
GUY Fabrice <fabrice.guy@rte-france.com>
2026-01-05 08:23:55 +0100

    tests: increase verbosity

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,12 @@
+import sys
+
 import pytest
 import wx
 
+
 @pytest.fixture(scope="session", autouse=True)
 def wx_app():
+    sys.argv = [sys.argv[0]]  # clear args to avoid interference
     """Create a wx.App instance for all tests"""
     app = wx.App(False)  # False = don't redirect stdout/stderr
     yield app
