7f6d8c04fc6ac5d59cdfdcd4b5d6cd004d9a013e
GUY Fabrice <fabrice.guy@rte-france.com>
2026-01-05 20:38:49 +0100

    tests: more tests + check input errors

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -16,6 +16,7 @@ import time
 @pytest.mark.parametrize(
     ("args", "expect"),
     [
+        # TODO: distinguish no value -> default_value, value UNSET, and empty string
         ("", "S:[no value]"),
         ("42", "S:[42]"),
         ("\N{SNOWMAN}", "S:[\N{SNOWMAN}]"),
@@ -53,7 +54,7 @@ def test_string_option(tmp_path, mocker, args, expect):
     [
         ("", "I:[84]"),
         ("23", "I:[46]"),
-        # ("x", "Error: Invalid value for '--i': 'x' is not a valid integer."),
+        ("x", "'x' is not a valid integer."),
     ],
 )
 def test_int_option(tmp_path, mocker, args, expect):
@@ -75,6 +76,10 @@ def test_int_option(tmp_path, mocker, args, expect):
         guick = original_init(ctx)
         guick.cmd_panels["cli"].entries["i"].SetValue(args)
         guick.cmd_panels["cli"].on_ok_button(None)
+        error = guick.cmd_panels["cli"].text_errors["i"].GetLabel()
+        if error:
+            logger.info(error)
+            guick.cmd_panels["cli"].on_close_button(None)
         return guick
     mocker.patch("guick.gui.Guick", init_gui)
     # mocker.patch("guick.gui.Guick.on_close_buttton", lambda: pass)
@@ -91,7 +96,7 @@ def test_int_option(tmp_path, mocker, args, expect):
             "821592c1-c50e-4971-9cd6-e89dc6832f86",
             "U:[821592c1-c50e-4971-9cd6-e89dc6832f86]",
         ),
-        # ("x", "Error: Invalid value for '--u': 'x' is not a valid UUID."),
+        ("x", "'x' is not a valid UUID."),
     ],
 )
 def test_uuid_option(tmp_path, mocker, args, expect):
@@ -115,6 +120,10 @@ def test_uuid_option(tmp_path, mocker, args, expect):
         guick = original_init(ctx)
         guick.cmd_panels["cli"].entries["u"].SetValue(args)
         guick.cmd_panels["cli"].on_ok_button(None)
+        error = guick.cmd_panels["cli"].text_errors["u"].GetLabel()
+        if error:
+            logger.info(error)
+            guick.cmd_panels["cli"].on_close_button(None)
         return guick
     mocker.patch("guick.gui.Guick", init_gui)
     # mocker.patch("guick.gui.Guick.on_close_buttton", lambda: pass)
@@ -128,7 +137,7 @@ def test_uuid_option(tmp_path, mocker, args, expect):
     [
         ("", "F:[42.0]"),
         ("23.5", "F:[23.5]"),
-        # ("x", "Error: Invalid value for '--f': 'x' is not a valid float."),
+        ("x", "'x' is not a valid float."),
     ],
 )
 def test_float_option(tmp_path, mocker, args, expect):
@@ -150,6 +159,10 @@ def test_float_option(tmp_path, mocker, args, expect):
         guick = original_init(ctx)
         guick.cmd_panels["cli"].entries["f"].SetValue(args)
         guick.cmd_panels["cli"].on_ok_button(None)
+        error = guick.cmd_panels["cli"].text_errors["f"].GetLabel()
+        if error:
+            logger.info(error)
+            guick.cmd_panels["cli"].on_close_button(None)
         return guick
     mocker.patch("guick.gui.Guick", init_gui)
     # mocker.patch("guick.gui.Guick.on_close_buttton", lambda: pass)
@@ -284,11 +297,142 @@ def test_file_option(tmp_path, mocker):
     assert "Hello World" in (tmp_path / "logfile.log").read_text(encoding="utf-8")
 
 
-def test_choice_option(tmp_path, mocker):
+def test_path_option(tmp_path, mocker):
+    @click.command(cls=gui.CommandGui)
+    @click.option("-O", type=click.Path(file_okay=False, exists=True, writable=True))
+    def write_to_dir(o):
+        with open(tmp_path / o / "foo.txt", "wb") as f:
+            f.write(b"meh\n")
+
+    os.mkdir(tmp_path / "test")
+
+    logger.remove()
+    logger.add(
+        tmp_path / "logfile.log",
+        level="INFO",
+    )
+    mocker.patch("wx.App")
+    mocker.patch("wx.App.MainLoop")
+
+    original_init = gui.Guick
+    def init_gui(ctx, size=None):
+        guick = original_init(ctx)
+        logger.info(list(guick.cmd_panels.keys()))
+        guick.cmd_panels["write-to-dir"].entries["o"].SetValue(str(tmp_path / "test"))
+        guick.cmd_panels["write-to-dir"].on_ok_button(None)
+        return guick
+    mocker.patch("guick.gui.Guick", init_gui)
+    # mocker.patch("guick.gui.Guick.on_close_buttton", lambda: pass)
+    with pytest.raises(SystemExit):
+        write_to_dir()
+    assert "meh" in (tmp_path / "test" / "foo.txt").read_text(encoding="utf-8")
+
+    def init_gui_second(ctx, size=None):
+        guick = original_init(ctx)
+        logger.info(list(guick.cmd_panels.keys()))
+        guick.cmd_panels["write-to-dir"].entries["o"].SetValue(str(tmp_path / "test" / "foo.txt"))
+        guick.cmd_panels["write-to-dir"].on_ok_button(None)
+        error = guick.cmd_panels["write-to-dir"].text_errors["o"].GetLabel()
+        if error:
+            logger.info(error)
+            guick.cmd_panels["write-to-dir"].on_close_button(None)
+        return guick
+    mocker.patch("guick.gui.Guick", init_gui_second)
+    # mocker.patch("guick.gui.Guick.on_close_buttton", lambda: pass)
+    with pytest.raises(SystemExit):
+        write_to_dir()
+    assert "is a file" in (tmp_path / "logfile.log").read_text(encoding="utf-8")
+
+
+def test_path_option_2(tmp_path, mocker):
+    @click.command(cls=gui.CommandGui)
+    @click.option("-f", type=click.Path(exists=True))
+    def showtype(f):
+        logger.info(f"is_file={os.path.isfile(f)}")
+        logger.info(f"is_dir={os.path.isdir(f)}")
+
+    logger.remove()
+    logger.add(
+        tmp_path / "logfile.log",
+        level="INFO",
+    )
+    mocker.patch("wx.App")
+    mocker.patch("wx.App.MainLoop")
+
+    original_init = gui.Guick
+    def init_gui(ctx, size=None):
+        guick = original_init(ctx)
+        guick.cmd_panels["showtype"].entries["f"].SetValue("xxx")
+        guick.cmd_panels["showtype"].on_ok_button(None)
+        error = guick.cmd_panels["showtype"].text_errors["f"].GetLabel()
+        if error:
+            logger.info(error)
+            guick.cmd_panels["showtype"].on_close_button(None)
+        return guick
+    mocker.patch("guick.gui.Guick", init_gui)
+    with pytest.raises(SystemExit):
+        showtype()
+    assert "does not exist" in (tmp_path / "logfile.log").read_text(encoding="utf-8")
+
+    def init_gui_new(ctx, size=None):
+        guick = original_init(ctx)
+        guick.cmd_panels["showtype"].entries["f"].SetValue(".")
+        guick.cmd_panels["showtype"].on_ok_button(None)
+        return guick
+    mocker.patch("guick.gui.Guick", init_gui_new)
+
+    with pytest.raises(SystemExit):
+        showtype()
+    assert "is_file=False" in (tmp_path / "logfile.log").read_text(encoding="utf-8")
+    assert "is_dir=True" in (tmp_path / "logfile.log").read_text(encoding="utf-8")
+
+
+@pytest.mark.parametrize(
+    ("args", "expect"),
+    [
+        ("xxx", "exists=False"),
+        (".", "exists=True"),
+    ],
+)
+def test_path_option_3(tmp_path, mocker, args, expect):
+    @click.command(cls=gui.CommandGui)
+    @click.option("-f", type=click.Path())
+    def exists(f):
+        logger.info(f"exists={os.path.exists(f)}")
+
+    logger.remove()
+    logger.add(
+        tmp_path / "logfile.log",
+        level="INFO",
+    )
+    mocker.patch("wx.App")
+    mocker.patch("wx.App.MainLoop")
+
+    original_init = gui.Guick
+    def init_gui_new(ctx, size=None):
+        guick = original_init(ctx)
+        guick.cmd_panels["exists"].entries["f"].SetValue(args)
+        guick.cmd_panels["exists"].on_ok_button(None)
+        return guick
+    mocker.patch("guick.gui.Guick", init_gui_new)
+
+    with pytest.raises(SystemExit):
+        exists()
+    assert expect in (tmp_path / "logfile.log").read_text(encoding="utf-8")
+
+
+@pytest.mark.parametrize(
+    ("args", "expect"),
+    [
+        ("foo", "S:[foo]"),
+        ("meh", "'meh' is not one of 'foo', 'bar', 'baz'."),
+    ],
+)
+def test_choice_option(tmp_path, mocker, args, expect):
     @click.command(cls=gui.CommandGui)
     @click.option("--method", type=click.Choice(["foo", "bar", "baz"]))
     def cli(method):
-        logger.info(method)
+        logger.info(f"S:[{method}]")
 
     logger.remove()
     logger.add(
@@ -301,21 +445,32 @@ def test_choice_option(tmp_path, mocker):
     original_init = gui.Guick
     def init_gui(ctx, size=None):
         guick = original_init(ctx)
-        guick.cmd_panels["cli"].entries["method"].SetValue("foo")
+        guick.cmd_panels["cli"].entries["method"].SetValue(args)
         guick.cmd_panels["cli"].on_ok_button(None)
+        error = guick.cmd_panels["cli"].text_errors["method"].GetLabel()
+        if error:
+            logger.info(error)
+            guick.cmd_panels["cli"].on_close_button(None)
         return guick
     mocker.patch("guick.gui.Guick", init_gui)
     # mocker.patch("guick.gui.Guick.on_close_buttton", lambda: pass)
     with pytest.raises(SystemExit):
         cli()
-    assert "foo" in (tmp_path / "logfile.log").read_text(encoding="utf-8")
+    assert expect in (tmp_path / "logfile.log").read_text(encoding="utf-8")
 
 
-def test_choice_argument(tmp_path, mocker):
+@pytest.mark.parametrize(
+    ("args", "expect"),
+    [
+        ("foo", "S:[foo]"),
+        ("meh", "'meh' is not one of 'foo', 'bar', 'baz'."),
+    ],
+)
+def test_choice_argument(tmp_path, mocker, args, expect):
     @click.command(cls=gui.CommandGui)
     @click.argument("method", type=click.Choice(["foo", "bar", "baz"]))
     def cli(method):
-        logger.info(method)
+        logger.info(f"S:[{method}]")
 
     logger.remove()
     logger.add(
@@ -328,71 +483,115 @@ def test_choice_argument(tmp_path, mocker):
     original_init = gui.Guick
     def init_gui(ctx, size=None):
         guick = original_init(ctx)
-        guick.cmd_panels["cli"].entries["method"].SetValue("foo")
+        guick.cmd_panels["cli"].entries["method"].SetValue(args)
         guick.cmd_panels["cli"].on_ok_button(None)
+        error = guick.cmd_panels["cli"].text_errors["method"].GetLabel()
+        if error:
+            logger.info(error)
+            guick.cmd_panels["cli"].on_close_button(None)
         return guick
     mocker.patch("guick.gui.Guick", init_gui)
     # mocker.patch("guick.gui.Guick.on_close_buttton", lambda: pass)
     with pytest.raises(SystemExit):
         cli()
-    assert "foo" in (tmp_path / "logfile.log").read_text(encoding="utf-8")
+    assert expect in (tmp_path / "logfile.log").read_text(encoding="utf-8")
 
 
-# @pytest.mark.skipif(click.__version__ <= "8.1.8", reason="requires click 9.0 or higher")
-# def test_choice_argument_enum(tmp_path, mocker):
-#     class MyEnum(str, enum.Enum):
-#         FOO = "foo-value"
-#         BAR = "bar-value"
-#         BAZ = "baz-value"
-# 
-#     @click.command(cls=gui.CommandGui)
-#     @click.argument("method", type=click.Choice(MyEnum, case_sensitive=False))
-#     def cli(method: MyEnum):
-#         assert isinstance(method, MyEnum)
-#         logger.info(method)
-# 
-#     logger.remove()
-#     logger.add(
-#         tmp_path / "logfile.log",
-#         level="INFO",
-#     )
-# 
-#     original_init = gui.Guick
-#     def init_gui(ctx, size=None):
-#         guick = original_init(ctx)
-#         guick.cmd_panels["cli"].entries["method"].SetValue("foo")
-#         guick.cmd_panels["cli"].on_ok_button(None)
-#         return guick
-#     mocker.patch("guick.gui.Guick", init_gui)
-#     # mocker.patch("guick.gui.Guick.on_close_buttton", lambda: pass)
-#     with pytest.raises(SystemExit):
-#         cli()
-#     assert "foo-value" in (tmp_path / "logfile.log").read_text(encoding="utf-8")
+@pytest.mark.parametrize(
+    ("args", "expect"),
+    [
+        ("foo", "S:[foo-value]"),
+        ("meh", "'meh' is not one of 'foo', 'bar', 'baz'."),
+    ],
+)
+@pytest.mark.skipif(click.__version__ <= "8.1.8", reason="requires click 8.1.8 or higher")
+def test_choice_argument_enum(tmp_path, mocker, args, expect):
+    class MyEnum(str, enum.Enum):
+        FOO = "foo-value"
+        BAR = "bar-value"
+        BAZ = "baz-value"
+
+    @click.command(cls=gui.CommandGui)
+    @click.argument("method", type=click.Choice(MyEnum, case_sensitive=False))
+    def cli(method: MyEnum):
+        assert isinstance(method, MyEnum)
+        # in the original click test, there is a call to click.echo without .value
+        # TODO check why click.echo and logger.info are not the same
+        logger.info(f"S:[{method.value}]")
+
+    logger.remove()
+    logger.add(
+        tmp_path / "logfile.log",
+        level="INFO",
+    )
+    mocker.patch("wx.App")
+    mocker.patch("wx.App.MainLoop")
+
+    original_init = gui.Guick
+    def init_gui(ctx, size=None):
+        guick = original_init(ctx)
+        guick.cmd_panels["cli"].entries["method"].SetValue(args)
+        guick.cmd_panels["cli"].on_ok_button(None)
+        error = guick.cmd_panels["cli"].text_errors["method"].GetLabel()
+        if error:
+            logger.info(error)
+            guick.cmd_panels["cli"].on_close_button(None)
+        return guick
+    mocker.patch("guick.gui.Guick", init_gui)
+    # mocker.patch("guick.gui.Guick.on_close_buttton", lambda: pass)
+    with pytest.raises(SystemExit):
+        cli()
+    assert expect in (tmp_path / "logfile.log").read_text(encoding="utf-8")
 
 
-# @pytest.mark.xfail(reason="see how to handle errors")
-# def test_choice_option_nok(tmp_path, mocker):
-#     @click.command(cls=gui.CommandGui)
-#     @click.option("--method", type=click.Choice(["foo", "bar", "baz"]))
-#     def cli(method):
-#         logger.info(method)
-# 
-#     logger.remove()
-#     logger.add(
-#         tmp_path / "logfile.log",
-#         level="INFO",
-#     )
-# 
-#     original_init = gui.Guick
-#     def init_gui(ctx, size=None):
-#         guick = original_init(ctx)
-#         guick.cmd_panels["cli"].entries["method"].SetValue("meh")
-#         guick.cmd_panels["cli"].on_ok_button(None)
-#         return guick
-#     mocker.patch("guick.gui.Guick", init_gui)
-#     # mocker.patch("guick.gui.Guick.on_close_buttton", lambda: pass)
-#     with pytest.raises(SystemExit):
-#         cli()
+@pytest.mark.parametrize(
+    ("args", "expect"),
+    [
+        ("foo", "S:[foo]"),
+        ("meh", "'meh' is not one of 'foo', 'bar', 'baz'."),
+    ],
+)
+@pytest.mark.skipif(click.__version__ <= "8.1.8", reason="requires click 8.1.8 or higher")
+def test_choice_argument_custom_type(tmp_path, mocker, args, expect):
+    class MyClass:
+        def __init__(self, value: str) -> None:
+            self.value = value
+
+        def __str__(self) -> str:
+            return self.value
+
+    @click.command(cls=gui.CommandGui)
+    @click.argument("method", type=click.Choice([MyClass("foo"), MyClass("bar"), MyClass("baz")]))
+    def cli(method: MyClass):
+        assert isinstance(method, MyClass)
+        # in the original click test, there is a call to click.echo without .value
+        # TODO check why click.echo and logger.info are not the same
+        logger.info(f"S:[{method}]")
+
+    logger.remove()
+    logger.add(
+        tmp_path / "logfile.log",
+        level="INFO",
+    )
+    mocker.patch("wx.App")
+    mocker.patch("wx.App.MainLoop")
+
+    original_init = gui.Guick
+    def init_gui(ctx, size=None):
+        guick = original_init(ctx)
+        guick.cmd_panels["cli"].entries["method"].SetValue(args)
+        guick.cmd_panels["cli"].on_ok_button(None)
+        error = guick.cmd_panels["cli"].text_errors["method"].GetLabel()
+        if error:
+            logger.info(error)
+            guick.cmd_panels["cli"].on_close_button(None)
+        return guick
+    mocker.patch("guick.gui.Guick", init_gui)
+    # mocker.patch("guick.gui.Guick.on_close_buttton", lambda: pass)
+    with pytest.raises(SystemExit):
+        cli()
+    assert expect in (tmp_path / "logfile.log").read_text(encoding="utf-8")
+
 
 def test_datetime_option_default(tmp_path, mocker):
     @click.command(cls=gui.CommandGui)
@@ -447,92 +646,6 @@ def test_datetime_option_custom(tmp_path, mocker):
     assert "2010-06-05T00:00:00" in (tmp_path / "logfile.log").read_text(encoding="utf-8")
 
 
-def test_path_option(tmp_path, mocker):
-    @click.command(cls=gui.CommandGui)
-    @click.option("-O", type=click.Path(file_okay=False, exists=True, writable=True))
-    def write_to_dir(o):
-        with open(tmp_path / o / "foo.txt", "wb") as f:
-            f.write(b"meh\n")
 
-    os.mkdir(tmp_path / "test")
 
-    logger.remove()
-    logger.add(
-        tmp_path / "logfile.log",
-        level="INFO",
-    )
-    mocker.patch("wx.App")
-    mocker.patch("wx.App.MainLoop")
 
-    original_init = gui.Guick
-    def init_gui(ctx, size=None):
-        guick = original_init(ctx)
-        logger.info(list(guick.cmd_panels.keys()))
-        guick.cmd_panels["write-to-dir"].entries["o"].SetValue(str(tmp_path / "test"))
-        guick.cmd_panels["write-to-dir"].on_ok_button(None)
-        return guick
-    mocker.patch("guick.gui.Guick", init_gui)
-    # mocker.patch("guick.gui.Guick.on_close_buttton", lambda: pass)
-    with pytest.raises(SystemExit):
-        write_to_dir()
-
-def test_path_option_2(tmp_path, mocker):
-    @click.command(cls=gui.CommandGui)
-    @click.option("-f", type=click.Path(exists=True))
-    def showtype(f):
-        logger.info(f"is_file={os.path.isfile(f)}")
-        logger.info(f"is_dir={os.path.isdir(f)}")
-
-    logger.remove()
-    logger.add(
-        tmp_path / "logfile.log",
-        level="INFO",
-    )
-    mocker.patch("wx.App")
-    mocker.patch("wx.App.MainLoop")
-
-    original_init = gui.Guick
-    def init_gui_new(ctx, size=None):
-        guick = original_init(ctx)
-        guick.cmd_panels["showtype"].entries["f"].SetValue(".")
-        guick.cmd_panels["showtype"].on_ok_button(None)
-        return guick
-    mocker.patch("guick.gui.Guick", init_gui_new)
-
-    with pytest.raises(SystemExit):
-        showtype()
-    assert "is_file=False" in (tmp_path / "logfile.log").read_text(encoding="utf-8")
-    assert "is_dir=True" in (tmp_path / "logfile.log").read_text(encoding="utf-8")
-
-@pytest.mark.parametrize(
-    ("args", "expect"),
-    [
-        ("xxx", "exists=False"),
-        (".", "exists=True"),
-    ],
-)
-def test_path_option_3(tmp_path, mocker, args, expect):
-    @click.command(cls=gui.CommandGui)
-    @click.option("-f", type=click.Path())
-    def exists(f):
-        logger.info(f"exists={os.path.exists(f)}")
-
-    logger.remove()
-    logger.add(
-        tmp_path / "logfile.log",
-        level="INFO",
-    )
-    mocker.patch("wx.App")
-    mocker.patch("wx.App.MainLoop")
-
-    original_init = gui.Guick
-    def init_gui_new(ctx, size=None):
-        guick = original_init(ctx)
-        guick.cmd_panels["exists"].entries["f"].SetValue(args)
-        guick.cmd_panels["exists"].on_ok_button(None)
-        return guick
-    mocker.patch("guick.gui.Guick", init_gui_new)
-
-    with pytest.raises(SystemExit):
-        exists()
-    assert expect in (tmp_path / "logfile.log").read_text(encoding="utf-8")
